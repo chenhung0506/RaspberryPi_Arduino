680a26fd3a88b6c22d4935da335904cf0dec196f
chenhung <chenhung0506@gmail.com>
2023-04-29 17:52:18 +0800

    upgrade

--- PythonProject/RFID_trigger_Buzzer_LCD.py
+++ PythonProject/RFID_trigger_Buzzer_LCD.py
@@ -58,8 +58,39 @@ def button_play():
             lcd.cursor_pos = (0, 0)
             lcd.write_string(ip)
 
+def set_angle(angle):
+    # 選擇控制伺服馬達的GPIO引腳
+    servo_pin = 18
+    # 使用BOARD模式
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(servo_pin, GPIO.OUT)
+
+    # 設置PWM頻率為50Hz
+    pwm = GPIO.PWM(servo_pin, 50)
+    pwm.start(0)
+
+    duty_cycle = angle / 18 + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    GPIO.output(servo_pin, False)
+    pwm.ChangeDutyCycle(0)
+
+try:
+    # 旋轉伺服馬達
+    set_angle(0)  # 第一次先關門
+    while True:
+        set_angle(90)  # 開門
+        time.sleep(2)
+        # set_angle(180)
+        set_angle(0)  # 關門
+        time.sleep(2)
+
+
 if __name__ == '__main__':
     t1 = threading.Thread(target=rfid_play)
     t1.start()
     t2 = threading.Thread(target=button_play)
     t2.start()
+    t3 = threading.Thread(target=set_angle)
+    t3.start()
