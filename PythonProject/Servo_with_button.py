502034b5a443c950a4055a7e7c4ddf463ee33390
chenhung <chenhung0506@gmail.com>
2023-04-29 16:02:04 +0800

    update

--- PythonProject/Servo_with_button.py
+++ PythonProject/Servo_with_button.py
@@ -4,24 +4,19 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 #Set Button and LED pins
-Button = 26
-LED = 28
+Button = 40
+LED = 38
 #Setup Button and LED
 GPIO.setup(Button,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED,GPIO.OUT)
-#flag = 0
+flag = 0
 
 while True:
     button_state = GPIO.input(Button)
     print(button_state)
-    if button_state == 0:
-        GPIO.output(LED,GPIO.HIGH)
-    else:
-        GPIO.output(LED,GPIO.LOW)
-    sleep(0.2)
-    '''
+
     if button_state==0:
-        sleep(0.5)
+        sleep(0.3)
         if flag==0:
             flag=1
         else:
@@ -30,4 +25,4 @@ while True:
         GPIO.output(LED,GPIO.HIGH)
     else:
         GPIO.output(LED,GPIO.LOW)  
-    ''' 
+
