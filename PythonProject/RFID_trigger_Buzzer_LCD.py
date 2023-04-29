898f4f7fe5081f2bb3d027638c29fa2d5768199e
chenhung <chenhung0506@gmail.com>
2023-04-29 18:02:22 +0800

    upgrade

--- PythonProject/RFID_trigger_Buzzer_LCD.py
+++ PythonProject/RFID_trigger_Buzzer_LCD.py
@@ -58,34 +58,42 @@ def button_play():
             lcd.cursor_pos = (0, 0)
             lcd.write_string(ip)
 
-# def set_angle(angle):
-#     # 選擇控制伺服馬達的GPIO引腳
-#     servo_pin = 18
-#     # 使用BOARD模式
-#     GPIO.setmode(GPIO.BOARD)
-#     GPIO.setup(servo_pin, GPIO.OUT)
+# 選擇控制伺服馬達的GPIO引腳
+servo_pin = 18
 
-#     # 設置PWM頻率為50Hz
-#     pwm = GPIO.PWM(servo_pin, 50)
-#     pwm.start(0)
+# 使用BOARD模式
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servo_pin, GPIO.OUT)
 
-#     duty_cycle = angle / 18 + 2
-#     GPIO.output(servo_pin, True)
-#     pwm.ChangeDutyCycle(duty_cycle)
-#     time.sleep(1)
-#     GPIO.output(servo_pin, False)
-#     pwm.ChangeDutyCycle(0)
+# 設置PWM頻率為50Hz
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(0)
 
-# def excute_angle():
-#     try:
-#         # 旋轉伺服馬達
-#         set_angle(0)  # 第一次先關門
-#         while True:
-#             set_angle(90)  # 開門
-#             time.sleep(2)
-#             # set_angle(180)
-#             set_angle(0)  # 關門
-#             time.sleep(2)
+def set_angle(angle):
+    duty_cycle = angle / 18 + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    GPIO.output(servo_pin, False)
+    pwm.ChangeDutyCycle(0)
+
+def excute_angle():
+    try:
+        # 旋轉伺服馬達
+        set_angle(0)  # 第一次先關門
+        while True:
+            set_angle(90)  # 開門
+            time.sleep(2)
+            # set_angle(180)
+            set_angle(0)  # 關門
+            time.sleep(2)
+
+    finally:
+        pwm.stop()
+        GPIO.cleanup()
+
+
+
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=rfid_play)
